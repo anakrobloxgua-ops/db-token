50b0fa1e23177e3277a12b64a51e4f882170d67c
DevKallz <anakrobloxgua@gmail.com>
2026-04-01 12:53:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const CryptoJS = require('crypto-js');
-
-const SECRET = "ZmZ_S3cr3t_2026";
-
-const tokenParts = [
-  ["8583548382:", "AAFzMhPRVPxYCtuME", "kXxi6vZlh2qmDc1PMU"]
-];
-
-const tokens = tokenParts.map(parts => {
-  const full = parts.join('');
-  const bytes = CryptoJS.AES.decrypt(full, SECRET);
-  return bytes.toString(CryptoJS.enc.Utf8);
-});
-
-console.log("Tokens:", tokens);
+{
+  "tokens": [
+    "8583548382:AAFzMhPRVPxYCtuMEkXxi6vZlh2qmDc1PMU"
+  ]
+}
